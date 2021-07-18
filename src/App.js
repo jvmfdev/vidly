87574fd9ab8fcbd7cae3cc2735605b01b8794506
Juan V Miranda <jvmiranda86@outlook.com>
2021-07-17 19:02:46 -0500

    Movies Navbar and Like Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
-import React, { Component } from 'react';
-import Movies from './components/movies';
+import "./App.css";
+import React, { Component } from "react";
+import Movies from "./components/movies";
+
 class App extends Component {
-  render(){
-  return (
-    <div className = "container">
-      <Movies/>
-    </div>
-  );
+  render() {
+    return (
+      <div className="container">
+        <Movies />
+      </div>
+    );
   }
 }
 
